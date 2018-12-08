5353c1ed4681c07174a8f567d5c01131baecfa06
Charlie <charlesbadjr@gmail.com>
2018-12-08 13:01:13 -0700

    reactrouterdom not for react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import flatEarth from './Assets/flatEarth.png';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={ flatEarth } className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            International Space Station 
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <button large > Launch
+            </button >  
+          </div>
         </header>
       </div>
     );
